4e4589eb4ee9f6613aec15a9eb685448a6461863
Sr.Lechuga <jonattanlima@gmail.com>
2023-05-17 02:09:37 -0300

    Guest statystics with style

--- codigo/invitado-principal.js
+++ codigo/invitado-principal.js
@@ -1,5 +1,15 @@
 const btnIngresarDatos = document.querySelector(".main-option.ingresar");
+      btnEliminarDatos = document.querySelector(".main-option.eliminar"),
+      btnConsultarEstadisticas = document.querySelector(".main-option.consultar");
 
 btnIngresarDatos.addEventListener("click", () => {
     window.location.assign("gestionar-informacion.html");
+});
+
+btnEliminarDatos.addEventListener("click", () => {
+    window.location.assign("gestionar-informacion.html");
+});
+
+btnConsultarEstadisticas.addEventListener("click", () => {
+    window.location.assign("estadistica-simple.html");
 });
